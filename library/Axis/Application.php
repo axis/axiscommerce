d72c30345dea1a47e3551fe1e86ae67b3e1df551
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2010-10-06 16:38:06 +0300

    Axis version updated to 0.7

--- library/Axis/Application.php
+++ library/Axis/Application.php
@@ -1,22 +1,22 @@
 <?php
 /**
  * Axis
- * 
+ *
  * This file is part of Axis.
- * 
+ *
  * Axis is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Axis is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Axis.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * @category    Axis
  * @package     Axis_Core
  * @copyright   Copyright 2008-2010 Axis
@@ -113,7 +113,7 @@ class Axis_Application extends Zend_Application
 
     /**
      *
-     * @return array 
+     * @return array
      */
     public function getNamespaces()
     {
@@ -145,6 +145,6 @@ class Axis_Application extends Zend_Application
      */
     public function getVersion()
     {
-        return '0.6.5';
+        return '0.7';
     }
 }
