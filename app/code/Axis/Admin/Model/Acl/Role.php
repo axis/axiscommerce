91939114bdfc679b5aa396450965c75fcdba4cd2
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2010-10-18 12:18:16 +0300

    Acl getParents fixed

--- app/code/Axis/Admin/Model/Acl/Role.php
+++ app/code/Axis/Admin/Model/Acl/Role.php
@@ -1,22 +1,22 @@
 <?php
 /**
  * Axis
- * 
+ *
  * This file is part of Axis.
- * 
+ *
  * Axis is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Axis is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Axis.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * @category    Axis
  * @package     Axis_Admin
  * @copyright   Copyright 2008-2010 Axis
@@ -24,7 +24,7 @@
  */
 
 /**
- * 
+ *
  * @category    Axis
  * @package     Axis_Admin
  * @subpackage  Model
@@ -34,7 +34,7 @@ class Axis_Admin_Model_Acl_Role extends Axis_Db_Table
 {
     protected $_name = 'admin_acl_role'; //aar
     protected $_rolesTree = null;
-        
+
     private function _initTree()
     {
         if (null !== $this->_rolesTree) {
@@ -46,7 +46,7 @@ class Axis_Admin_Model_Acl_Role extends Axis_Db_Table
                 array('child_id' => 'role_id')
             )
             ->query();
-        
+
         $rolesTree = array();
 
         while (($row = $stmt->fetch())) {
@@ -57,7 +57,7 @@ class Axis_Admin_Model_Acl_Role extends Axis_Db_Table
                 $rolesTree[$row['id']]['childs'][] = $row['child_id'];
             }
         }
-        
+
         foreach ($rolesTree as $id => $item) {
             if (!isset($item['childs'])) {
                 continue;
@@ -69,22 +69,22 @@ class Axis_Admin_Model_Acl_Role extends Axis_Db_Table
                 $rolesTree[$child]['parents'][] = $id;
             }
         }
-        
+
         $this->_rolesTree = $rolesTree;
     }
-    
+
     public function getRolesTree()
     {
         $this->_initTree();
         return $this->_rolesTree;
     }
-    
+
     public function getAllChilds($id)
     {
         $this->_initTree();
-        if (isset($this->_rolesTree[$id]['childs']) 
+        if (isset($this->_rolesTree[$id]['childs'])
             && sizeof($this->_rolesTree[$id]['childs'])) {
-            
+
             $childs = $this->_rolesTree[$id]['childs'];
             $subchilds = array();
             foreach ($childs as $childId) {
@@ -94,14 +94,14 @@ class Axis_Admin_Model_Acl_Role extends Axis_Db_Table
             }
             return array_merge($childs, $subchilds);
         }
-        
+
         return array();
     }
-    
+
     public function getAllParents($id)
     {
         $this->_initTree();
-        if (isset($this->_rolesTree[$id]['parents']) 
+        if (isset($this->_rolesTree[$id]['parents'])
             && sizeof($this->_rolesTree[$id]['parents'])) {
 
             $parents = $this->_rolesTree[$id]['parents'];
@@ -113,10 +113,10 @@ class Axis_Admin_Model_Acl_Role extends Axis_Db_Table
             }
             return array_merge($parents, $subparents);
         }
-        
+
         return array();
     }
-    
+
     public function getPossibleParents($id)
     {
         $childRoleIds = $this->getAllChilds($id, $this->getRolesTree());
@@ -135,17 +135,17 @@ class Axis_Admin_Model_Acl_Role extends Axis_Db_Table
      */
     public function getParents($roleId)
     {
-        return Axis::single('admin_acl_role_parent')->select('role_parent_id')
+        return Axis::single('admin/acl_role_parent')->select('role_parent_id')
             ->where('role_id = ?', $roleId)
             ->fetchCol();
     }
-    
+
     public function getName($id)
     {
         $this->_initTree();
         return $this->_rolesTree[$id]['role_name'];
     }
-    
+
     public function getRules($roleId)
     {
         return Axis::single('admin/acl_rule')->select(
@@ -154,10 +154,10 @@ class Axis_Admin_Model_Acl_Role extends Axis_Db_Table
             ->where('role_id = ?', $roleId)
             ->fetchAll();
     }
-    
+
     /**
      * Save parent roles
-     * 
+     *
      * @param $roleId integer
      * @param $parents array
      */
@@ -175,7 +175,7 @@ class Axis_Admin_Model_Acl_Role extends Axis_Db_Table
             ));
         }
     }
-    
+
     public function saveRules($roleId, $rules)
     {
         /* remove old rules */
@@ -183,7 +183,7 @@ class Axis_Admin_Model_Acl_Role extends Axis_Db_Table
         $modelAclRule->delete(
             $this->getAdapter()->quoteInto('role_id = ?', $roleId)
         );
-        
+
         /* insert allows */
         if (isset($rules['allow'])) {
             foreach ($rules['allow'] as $resourceId) {
