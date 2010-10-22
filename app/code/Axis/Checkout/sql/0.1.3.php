16d74256e79488b6a17a9cb8ac0193b667a768db
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2010-10-22 11:47:09 +0300

    Added foreign key to checkout_cart table (site_id)

---
+++ app/code/Axis/Checkout/sql/0.1.3.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Axis
+ *
+ * This file is part of Axis.
+ *
+ * Axis is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Axis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Axis.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category    Axis
+ * @package     Axis_Account
+ * @copyright   Copyright 2008-2010 Axis
+ * @license     GNU Public License V3.0
+ */
+
+class Axis_Checkout_Upgrade_0_1_3 extends Axis_Core_Model_Migration_Abstract
+{
+    protected $_version = '0.1.3';
+    protected $_info = 'upgrade';
+
+    public function up()
+    {
+        $installer = Axis::single('install/installer');
+
+        $installer->run("
+
+        ALTER TABLE `{$installer->getTable('checkout_cart')}`
+            ADD CONSTRAINT `FK_CHECKOUT_CART_SITE` FOREIGN KEY `FK_CHECKOUT_CART_SITE` (`site_id`)
+                REFERENCES `core_site` (`id`)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE;
+
+        ");
+    }
+
+    public function down()
+    {
+        $installer = Axis::single('install/installer');
+
+        $installer->run("
+
+        ALTER TABLE `{$installer->getTable('checkout_cart')}`
+            DROP FOREIGN KEY `FK_CHECKOUT_CART_SITE`;
+
+        ");
+    }
+}
