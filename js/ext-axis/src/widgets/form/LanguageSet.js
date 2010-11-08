dacf0879a77c3abdf8e827658b9a884741182db1
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2010-11-08 15:20:15 +0200

    Better reseting of tabs with errors

--- js/ext-axis/src/widgets/form/LanguageSet.js
+++ js/ext-axis/src/widgets/form/LanguageSet.js
@@ -279,7 +279,7 @@ Axis.form.LanguageSet = Ext.extend(Ext.form.TextField, {
                     var value = f.getValue();
                     f.validate();
                 } else {
-                    var value = this.value[f.name];
+                    var value = this.value ? this.value[f.name] : '';
                 }
                 if (value == '') {
                     failedLocales.push(f.locale);
@@ -538,6 +538,14 @@ Axis.form.LanguageSet = Ext.extend(Ext.form.TextField, {
     setRawValueForItem: function(val) {
         this.eachItem(function(item) {
             item.setRawValue(val);
+        });
+    },
+
+    clearInvalid: function() {
+        this.eachItem(function(item) {
+            if (item.clearInvalid) {
+                item.clearInvalid();
+            }
         });
     }
 });
