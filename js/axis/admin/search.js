ca2697cda3342204764610098b7eb9407a2c3e51
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2010-12-09 10:47:07 +0200

    Filters in search grid

--- js/axis/admin/search.js
+++ js/axis/admin/search.js
@@ -20,164 +20,122 @@
  * @license     GNU Public License V3.0
  */
 
-Ext.onReady(function (){
-    Ext.QuickTips.init();
-    var menuTime = today;
+Ext.onReady(function() {
 
-    var filters = new Ext.ux.grid.GridFilters({
-        filters: [
-            {type: 'numeric', dataIndex: 'id'},
-            {type: 'numeric', dataIndex: 'customer_id'},
-            {type: 'numeric', dataIndex: 'num_results'},
-            {type: 'numeric', dataIndex: 'hit'},
-            {type: 'string', dataIndex: 'customer_email'},
-            {type: 'string', dataIndex: 'query'},
-            {type: 'string', dataIndex: 'session_id'},
-            {type: 'date',   dataIndex: 'created_at', dateFormat: 'Y-m-d'}
-        ]
-    });
+    Ext.QuickTips.init();
 
     var storeSearch = new Ext.data.GroupingStore({
-        proxy: new Ext.data.HttpProxy({
-            url: Axis.getUrl('search/list')
-        }),
+        autoLoad: true,
+        beaseParams: {
+            limit: 25
+        },
         reader: new Ext.data.JsonReader({
                 root : 'data',
                 totalProperty: 'count',
-                id: 'id'
-            },
-            ['id', 'num_results', 'hit', 'session_id', 'created_at', 'query', 'customer_email', 'customer_id']
+                idProperty: 'id'
+            }, [
+                {name: 'id', type: 'int'},
+                {name: 'num_results', type: 'int'},
+                {name: 'hit', type: 'int'},
+                {name: 'session_id'},
+                {name: 'created_at', type: 'date', dateFormat: 'Y-m-d H:i:s'},
+                {name: 'query'},
+                {name: 'customer_email'},
+                {name: 'customer_id', type: 'int'}
+            ]
         ),
-        sortInfo: {field: 'created_at', direction: "DESC"},
+        sortInfo: {
+            field: 'id',
+            direction: 'DESC'
+        },
         remoteSort: true,
-        groupField:'query'
+        url: Axis.getUrl('search/list')
     });
 
     function renderCustomer(value, meta, record) {
-        var link = (Axis.getUrl('customer_index/index/customerId/.customerId.'))
-            .replace(/\.customerId\./, record.data.customer_id);
         if (value == null) {
-            value = ' Guest';
-            link = '#';
+            return 'Guest'.l();
         }
 
-        meta.attr = 'ext:qtip="goto ' + value + '"';
         return String.format(
-                '<a href="{1}" target="_blank">{0}</a>',
-                value, link);
+            '<a href="{1}" target="_blank">{0}</a>',
+            value,
+            Axis.getUrl('customer_index/index/customerId/' + record.data.customer_id)
+        );
     }
-    var selectModel = new Ext.grid.CheckboxSelectionModel();
 
-    var columnsSearch = new Ext.grid.ColumnModel([{
-        header: "Id".l(),
-        width: 30,
-        sortable: true,
-        dataIndex: 'id',
-        groupable:false
-    }, {
-        header: "Query".l(),
-        width: 300,
-        sortable: true,
-        dataIndex: 'query'
-    }, {
-        header: "Results".l(),
-        id:'product_name',
-        sortable: true,
-        dataIndex: 'num_results'
-    }, {
-        header: "Customer".l(),
-        width: 170,
-        sortable: true,
-        dataIndex: 'customer_email',
-        renderer: renderCustomer
-    }, {
-        header: "Hit".l(),
-        width: 30,
-        dataIndex: 'hit',
-        sortable: true,
-        groupable:false
-    }, {
-        header: "Created On".l(),
-        width: 145,
-        sortable: true,
-        dataIndex: 'created_at',
-        groupable:false
-    }]);
+    var columnsSearch = new Ext.grid.ColumnModel({
+        defaults: {
+            sortable: true,
+            table: 'sl'
+        },
+        columns: [{
+            dataIndex: 'id',
+            header: "Id".l(),
+            width: 90
+        }, {
+            dataIndex: 'query',
+            header: "Query".l(),
+            id: 'query',
+            table: 'slq',
+            filter: {
+                operator: 'LIKE'
+            }
+        }, {
+            header: "Results".l(),
+            dataIndex: 'num_results',
+            width: 90
+        }, {
+            header: "Customer".l(),
+            width: 190,
+            dataIndex: 'customer_email',
+            renderer: renderCustomer,
+            table: 'ac',
+            sortName: 'email',
+            filter: {
+                name: 'email',
+                operator: 'LIKE'
+            }
+        }, {
+            dataIndex: 'hit',
+            header: "Hit".l(),
+            table: 'slq',
+            width: 90
+        }, {
+            dataIndex: 'created_at',
+            header: "Created On".l(),
+            renderer: function(value) {
+                return Ext.util.Format.date(value) + ' ' + Ext.util.Format.date(value, 'H:i:s');
+            },
+            width: 130
+        }]
+    });
 
-    gridSearch = new Axis.grid.EditorGridPanel({
-        autoExpandColumn: 'product_name',
-        store: storeSearch,
+    var gridSearch = new Axis.grid.EditorGridPanel({
+        id: 'grid-search',
+        autoExpandColumn: 'query',
+        ds: storeSearch,
         cm: columnsSearch,
         view: new Ext.grid.GroupingView({
             emptyText: 'No records found'.l(),
             groupTextTpl: '{text} ({[values.rs.length]} {[values.rs.length > 1 ? "Items" : "Item"]})'
         }),
-        plugins:[
-            filters,
-            new Ext.ux.grid.Search({
-                mode:'local',
-                iconCls:false,
-                dateFormat:'Y-m-d',
-                width: 150,
-                minLength:2
-            })
-        ],
+        plugins:[new Axis.grid.Filter()],
         tbar: [{
-            text: 'Fast Filter'.l(),
-            menu : {
-                items: [{
-                    text: 'Time Options'.l(),
-                    checked: false,
-                    checkHandler: function(menuItem, chekced) {
-                        setFilterDate('created_at', 'gt', menuTime, chekced);
-                    },
-                    menu: {
-                        items:[{
-                            text: 'Today ({count})'.l('core', countToday),
-                            group: 'time',
-                            checked: false,
-                            checkHandler: function(menuItem, cheked) {
-                                if (cheked) menuTime = today;
-                            }
-                        }, {
-                            text: 'Last week ({count})'.l('core', countWeek),
-                            group: 'time',
-                            checked: false,
-                            checkHandler: function(menuItem, cheked) {
-                                 if (cheked) menuTime = week;
-                             }
-                        }, {
-                            text: 'Last month ({count})'.l('core', countMonth),
-                            group: 'time',
-                            checked: false ,
-                            checkHandler: function(menuItem, cheked){
-                                if (cheked) menuTime = month;
-                            }
-                        }]
-                    }
-                }, {
-                    text: 'Empty results ({count})'.l('core', countNull),
-                    checked: false,
-                    checkHandler: function(menuItem, checked) {
-                        setFilterNumeric('num_results', 'eq', 0, checked);
-                    }
-                }, '-', {
-                    text: 'Reset filter'.l(),
-                    handler:resetFilters
-                }
-            ]}
-        }, {
             text: 'Delete'.l(),
             icon: Axis.skinUrl + '/images/icons/delete.png',
             cls: 'x-btn-text-icon',
-            handler :function() {
-                if (!confirm('Are you sure?'.l())) {
+            handler: function() {
+                var selectedItems = Ext.getCmp('grid-search')
+                    .getSelectionModel()
+                    .getSelections();
+
+                if (!selectedItems.length || !confirm('Are you sure?'.l())) {
                     return;
                 }
+
                 var data = {};
-                var selectedItems = gridSearch.getSelectionModel().selections.items;
-                for (var i = 0; i < selectedItems.length; i++) {
-                    if (!selectedItems[i]['data']['id']) continue;
+                for (var i = 0, len = selectedItems.length; i < len; i++) {
                     data[i] = selectedItems[i]['data']['id'];
                 }
 
@@ -192,7 +150,7 @@ Ext.onReady(function (){
         }, '->', {
             text: 'Reload'.l(),
             handler: function() {
-                gridSearch.getStore().reload();
+                Ext.getCmp('grid-search').getStore().reload();
             },
             iconCls: 'btn-text-icon',
             icon: Axis.skinUrl + '/images/icons/refresh.png'
@@ -207,82 +165,4 @@ Ext.onReady(function (){
             gridSearch
         ]
     });
-
-    function setSearchQuery(id) {
-        var store = gridSearch.getStore();
-        store.lastOptions = {params:{start:0, limit:25}};
-        gridSearch.filters.filters.get('id').setActive(false);
-        gridSearch.filters.filters.get('id').setValue({'eq': id});
-        gridSearch.filters.filters.get('id').setActive(true);
-    }
-
-    function setCustomer(id) {
-        var store = gridSearch.getStore();
-        store.lastOptions = {params:{start:0, limit:25}};
-        gridSearch.filters.filters.get('customer_id').setActive(false);
-        gridSearch.filters.filters.get('customer_id').setValue({'eq': id});
-        gridSearch.filters.filters.get('customer_id').setActive(true);
-    }
-
-    gridSearch.render();
-
-    if (typeof(searchId) !== "undefined")
-        setSearchQuery(searchId);
-    else {
-        if (typeof(customerId) !== "undefined")
-            setCustomer(customerId);
-        else {
-            storeSearch.load({params:{start:0, limit:25}});
-        }
-    }
 });
-function resetFilters() {
-     gridSearch.filters.clearFilters();
-}
-// only for filter with type numeric
-function setFilterNumeric(field, condition, val, checked) {
-    var store = gridSearch.getStore();
-    store.lastOptions = {params:{start:0, limit:25}};
-    gridSearch.filters.filters.get(field).setActive(false);
-    switch(condition) {
-         case 'lt':
-             gridSearch.filters.filters.get(field).setValue({'lt': val});
-             break;
-         case 'gt':
-             gridSearch.filters.filters.get(field).setValue({'gt': val});
-             break;
-         default:
-             gridSearch.filters.filters.get(field).setValue({'eq': val});
-    }
-    gridSearch.filters.filters.get(field).setActive(checked);
-}
-// only for filter with type date
-//@params field name of colunm see dataIndex,
-function setFilterDate(field, condition, val, checked) {
-    var store = gridSearch.getStore();
-    store.lastOptions = {params:{start:0, limit:25}};
-    gridSearch.filters.filters.get(field).setActive(false);
-    var dt = new Date();
-    dt = Date.parseDate(val, "Y-m-d");
-    switch(condition) {
-        case 'lt':
-            gridSearch.filters.filters.get(field).setValue({'before': dt});
-            break;
-        case 'gt':
-            gridSearch.filters.filters.get(field).setValue({'after': dt});
-            break;
-        default:
-            gridSearch.filters.filters.get(field).setValue({'on': dt});
-    }
-    gridSearch.filters.filters.get(field).setActive(checked);
-}
-// only for filter with type string
-function setFilterString(field, val, checked) {
-    var store = gridSearch.getStore();
-    store.lastOptions = {params:{start:0, limit:25}};
-    gridSearch.filters.filters.get(field).setActive(false);
-    gridSearch.filters.filters.get(field).setValue(val); // val is a string
-    gridSearch.filters.filters.get(field).setActive(checked);
-}
-
-
