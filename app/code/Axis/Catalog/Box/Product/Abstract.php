37a7484cb00f219a72451c7d7cbe4d22c798e6a3
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2011-02-18 11:06:19 +0200

    Extra query removed from product box

--- app/code/Axis/Catalog/Box/Product/Abstract.php
+++ app/code/Axis/Catalog/Box/Product/Abstract.php
@@ -1,22 +1,22 @@
 <?php
 /**
  * Axis
- * 
+ *
  * This file is part of Axis.
- * 
+ *
  * Axis is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Axis is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Axis.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * @category    Axis
  * @package     Axis_Catalog
  * @subpackage  Axis_Catalog_Box
@@ -25,7 +25,7 @@
  */
 
 /**
- * 
+ *
  * @category    Axis
  * @package     Axis_Catalog
  * @subpackage  Axis_Catalog_Box
@@ -47,8 +47,15 @@ abstract class Axis_Catalog_Box_Product_Abstract extends Axis_Core_Box_Abstract
         return Zend_Controller_Front::getInstance()->getRequest()->getParam('product', 0);
     }
 
-    public function init() 
+    public function init()
     {
         $this->productId = $this->_getProductId();
+
+        if (Zend_Registry::isRegistered('catalog/current_product')) {
+            $product = Zend_Registry::get('catalog/current_product');
+            if (!$this->productId || $product->id == $this->productId) {
+                $this->product = $product;
+            }
+        }
     }
 }
