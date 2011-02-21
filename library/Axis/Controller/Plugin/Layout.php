75668b133e04221b2c8df6e64fa757c39c64a67d
Alexander Kras'ko <0m3r.mail@gmail.com>
2011-02-21 17:37:27 +0200

    blocks are initialized only when enabled layout

--- library/Axis/Controller/Plugin/Layout.php
+++ library/Axis/Controller/Plugin/Layout.php
@@ -245,9 +245,54 @@ class Axis_Controller_Plugin_Layout extends Zend_Layout_Controller_Plugin_Layout
      */
     public function postDispatch(Zend_Controller_Request_Abstract $request)
     {
+        $layout = $this->getLayout();
+        $helper = $this->getLayoutActionHelper();
+
+        // Return early if forward detected
+        if (!$request->isDispatched()
+            || $this->getResponse()->isRedirect()
+            || ($layout->getMvcSuccessfulActionOnly()
+                && (!empty($helper) && !$helper->isActionControllerSuccessful())))
+        {
+            return;
+        }
+
+        // Return early if layout has been disabled
+        if (!$layout->isEnabled()) {
+            return;
+        }
+
         $this->_initPages();
         $this->_initLayout();
         $this->_initBlockAssigns();
-        return parent::postDispatch($request);
+
+        $response   = $this->getResponse();
+        $content    = $response->getBody(true);
+        $contentKey = $layout->getContentKey();
+
+        if (isset($content['default'])) {
+            $content[$contentKey] = $content['default'];
+        }
+        if ('default' != $contentKey) {
+            unset($content['default']);
+        }
+
+        $layout->assign($content);
+
+        $fullContent = null;
+        $obStartLevel = ob_get_level();
+        try {
+            $fullContent = $layout->render();
+            $response->setBody($fullContent);
+        } catch (Exception $e) {
+            while (ob_get_level() > $obStartLevel) {
+                $fullContent .= ob_get_clean();
+            }
+            $request->setParam('layoutFullContent', $fullContent);
+            $request->setParam('layoutContent', $layout->content);
+            $response->setBody(null);
+            throw $e;
+        }
+
     }
 }
