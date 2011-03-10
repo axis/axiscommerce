bb5fb79e8bc9c3a241a1f459d3ed085f6a8fb37e
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2011-03-10 13:40:40 +0200

    Removed unnesessary checking for locale in url

--- app/code/Axis/Locale/controllers/IndexController.php
+++ app/code/Axis/Locale/controllers/IndexController.php
@@ -1,22 +1,22 @@
 <?php
 /**
  * Axis
- * 
+ *
  * This file is part of Axis.
- * 
+ *
  * Axis is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Axis is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Axis.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * @category    Axis
  * @package     Axis_Locale
  * @subpackage  Axis_Locale_Controller
@@ -25,14 +25,14 @@
  */
 
 /**
- * 
+ *
  * @category    Axis
  * @package     Axis_Locale
  * @subpackage  Axis_Locale_Controller
  * @author      Axis Core Team <core@axiscommerce.com>
  */
 class Axis_Locale_IndexController extends Axis_Core_Controller_Front
-{   
+{
     public function changeAction()
     {
         $locale = $this->_getParam('lang');
@@ -49,5 +49,5 @@ class Axis_Locale_IndexController extends Axis_Core_Controller_Front
         }
         $this->_redirect($redirectUrl);
     }
-    
+
 }
