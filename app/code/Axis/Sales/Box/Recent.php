8933e0f4652b0713f380b2a09863a3c674939d0b
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2011-05-23 11:19:04 +0300

    Orders should be ordered from newest to oldest

--- app/code/Axis/Sales/Box/Recent.php
+++ app/code/Axis/Sales/Box/Recent.php
@@ -1,22 +1,22 @@
 <?php
 /**
  * Axis
- * 
+ *
  * This file is part of Axis.
- * 
+ *
  * Axis is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Axis is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Axis.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * @category    Axis
  * @package     Axis_Sales
  * @subpackage  Axis_Sales_Box
@@ -25,7 +25,7 @@
  */
 
 /**
- * 
+ *
  * @category    Axis
  * @package     Axis_Sales
  * @subpackage  Axis_Sales_Box
@@ -35,12 +35,12 @@ class Axis_Sales_Box_Recent extends Axis_Account_Box_Abstract
 {
     protected $_title = 'Recent Orders';
     protected $_class = 'box-orders';
-    
+
     public function init()
     {
-        $orders = array_reverse(Axis::single('sales/order')
-            ->getOrdersByCustomer(Axis::getCustomerId())
-        );
+        $orders = Axis::single('sales/order')
+            ->getOrdersByCustomer(Axis::getCustomerId());
+
         if (!count($orders)) {
            return false;
         }
