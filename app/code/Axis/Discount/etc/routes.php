a1d705279f11e3f9df918a55eb50e21c5be5a8f8
Alexander Kras'ko <0m3r.mail@gmail.com>
2011-09-06 12:39:19 +0300

    admin/discount/* was refactored

---
+++ app/code/Axis/Discount/etc/routes.php
@@ -0,0 +1,33 @@
+<?php 
+/**
+ * Axis
+ * 
+ * This file is part of Axis.
+ * 
+ * Axis is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Axis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Axis.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @category    Axis
+ * @package     Axis_Location
+ * @copyright   Copyright 2008-2011 Axis
+ * @license     GNU Public License V3.0
+ */
+
+$router->addRoute('admin/discount', new Axis_Controller_Router_Route_Back(
+    'discount/:action/*',
+    array(
+        'module' => 'Axis_Discount',
+        'controller' => 'index',
+        'action' => 'index'
+    )
+), 'admin');
