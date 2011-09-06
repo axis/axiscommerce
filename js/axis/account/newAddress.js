8bd15e33dfda4dcde1a5c16c7fdbd395e9a9de1b
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2011-09-06 15:59:15 +0300

    ToggleZone fixed at customer address page

--- js/axis/account/newAddress.js
+++ js/axis/account/newAddress.js
@@ -1,28 +1,29 @@
 /**
  * Axis
- * 
+ *
  * This file is part of Axis.
- * 
+ *
  * Axis is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Axis is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Axis.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * @copyright   Copyright 2008-2011 Axis
  * @license     GNU Public License V3.0
  */
-$(document).ready(function(){
-    
-    var elZone = $('.input-zone');
-    
+
+$(document).ready(function() {
+
+    var elZone = $('select.input-zone');
+
     elZone.after(
         '<input type="text" class="'
         + elZone.attr('class') + ' input-text" name="'
@@ -30,10 +31,10 @@ $(document).ready(function(){
         + elZone.attr('id') + '"'
         + ' />'
     );
-        
-    function toggleZone(el)
-    {
-        var select = el, textfield = el.next();
+
+    function toggleZone(el) {
+        var select      = el,
+            textfield   = el.parent().find('input.input-zone');
 
         if (select.children().length) {
             select.insertAfter(textfield)
@@ -50,43 +51,18 @@ $(document).ready(function(){
         }
     }
     toggleZone(elZone);
-    
-    $('.input-country').change(function(){
 
-        elZone.removeOption(/./);
-
+    $('.input-country').change(function() {
+        elZone.html('');
         if (Zones[this.value]) {
-            elZone.addOption(Zones[this.value], false);
+            for (var id in Zones[this.value]) {
+                var zone    = Zones[this.value][id],
+                    option  = '<option label="' + zone + '" value="' + id + '">'
+                        + zone
+                        + '</option>';
+                elZone.append(option);
+            }
         }
         toggleZone(elZone);
     });
 });
-
-function form_send()
-{
-    $('#form_submit').attr('disabled', 'true')
-    $.ajax({
-        type: "POST",
-        url : Axis.getUrl('account/address-book/save', true),
-        data : $('#form-new-address').serialize(),
-        dataType: 'json',
-        success: function(response) {
-            $('#form_submit').removeAttr('disabled');
-            $("#form-new-address input").removeClass('error');
-            $('div.error').remove();
-            if (true === response) {
-                window.location = Axis.getUrl('account/address-book', true);
-            } else {
-                for (var i in response) {
-                    var messages = response[i];
-                    var message = "";
-                    $('#' + i).addClass('error');
-                    for (var j in messages) {
-                        message += messages[j] + '<br />';
-                    }
-                    $('#' + i).after('<div class="error">' + message + '</div>');
-                }
-            }
-        }
-    });
-}
