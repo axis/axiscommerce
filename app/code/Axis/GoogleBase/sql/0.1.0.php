5acd0778e20d81a4a69cdc0a7e9a37eb4715b4e6
Alexander Kras'ko <0m3r.mail@gmail.com>
2011-12-19 16:10:01 +0200

    gbase config  options was refactored

--- app/code/Axis/GoogleBase/sql/0.1.0.php
+++ app/code/Axis/GoogleBase/sql/0.1.0.php
@@ -32,15 +32,15 @@ class Axis_GoogleBase_Upgrade_0_1_0 extends Axis_Core_Model_Migration_Abstract
     {
         Axis::single('core/config_field')
             ->add('gbase', 'Google Base', null, null, array('translation_module' => 'Axis_GoogleBase'))
-            ->add('gbase/main/payment', 'Google Base/General/Payment', 'Discover,American Express,Visa,MasterCard,Check', 'multiple', 'Let your customers buy with all major credit cards', array('config_options' => 'Discover,American Express,Visa,MasterCard,Wire transfer,Check,Cash'))
+            ->add('gbase/main/payment', 'Google Base/General/Payment', Axis_GoogleBase_Model_Payment::getDeafultValue(), 'multiple', 'Let your customers buy with all major credit cards', array('model' => 'Axis_GoogleBase_Model_Payment'))
             ->add('gbase/main/notes', 'Payment notes', 'Google Checkout', 'string')
             ->add('gbase/main/application', 'Application', 'StoreArchitect-Axis-' . Axis::app()->getVersion(), 'string', 'Name of the application that last modified this item.\r\nAll applications should set this attribute whenever they insert or update an item. Recommended format : Organization-ApplicationName-Version')
             ->add('gbase/main/dryRun', 'dryRun', '0', 'bool', "Set 'Yes' for testing, 'No' for production")
-            ->add('gbase/main/link', 'Link products to', 'Website', 'select', "If you want to use GoogleBase pages as landing page for your items, or you  can't give the link to your webstore - select Google Base, otherwise - select Website.", array('config_options' => 'GoogleBase,Website'))
+            ->add('gbase/main/link', 'Link products to', Axis_GoogleBase_Model_LinkType::WEBSITE, 'select', "If you want to use GoogleBase pages as landing page for your items, or you  can't give the link to your webstore - select Google Base, otherwise - select Website.", array('model' => 'Axis_GoogleBase_Model_LinkType'))
             ->add('gbase/main/itemType', 'Item type', 'Products', 'string', 'Type of your products. Read this for more information http://code.google.com/apis/base/starting-out.html#ItemTypes')
             ->add('gbase/auth/login', 'Login', '', 'handler', 'Your google account to submit products', array('model' => 'Crypt'))
             ->add('gbase/auth/password', 'Password', '', 'handler', 'Password to google account', array('model' => 'Crypt'))
-            ->add('gbase/auth/connection', 'Connection type', 'AuthSub', 'select', 'Login type. For ClientLogin fill login and password fields. For AuthSub you will have to enter login and password manually', array('config_options' => 'ClientLogin,AuthSub'));
+            ->add('gbase/auth/connection', 'Connection type', Axis_GoogleBase_Model_ConnectionType::AUTH_SUB, 'select', 'Login type. For ClientLogin fill login and password fields. For AuthSub you will have to enter login and password manually', array('model' => 'Axis_GoogleBase_Model_ConnectionType'));
 
     }
 
