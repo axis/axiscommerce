2ba230b0dad7dd5be2cb377bf16869afdb407714
Alexander Kras'ko <0m3r.mail@gmail.com>
2012-04-17 17:48:39 +0300

    Item_Standard

--- app/code/Axis/Core/controllers/SandboxController.php
+++ app/code/Axis/Core/controllers/SandboxController.php
@@ -38,17 +38,14 @@ class SandboxController extends Axis_Core_Controller_Front
         
          Axis::single('core/config_field')
             ->add('shipping', 'Shipping Methods', null, null, array('translation_module' => 'Axis_Admin'))
-            ->add('shipping/Flat_Standard', 'Shipping Methods/Flat Rate Standard', null, null, array('translation_module' => 'Axis_ShippingFlat'))
-            ->add('shipping/Flat_Standard/enabled', 'Shipping Methods/Flat Rate Standard/Enabled', 1, 'radio', '', array('model'=> 'core/option_boolean', 'translation_module' => 'Axis_Core'))
-            ->add('shipping/Flat_Standard/geozone', 'Allowed Shipping Zone', '1', 'select', 'Shipping method will be available only for selected zone', array('model' => 'location/option_geozone', 'translation_module' => 'Axis_Admin'))
-            ->add('shipping/Flat_Standard/taxBasis', 'Tax Basis', '', 'select', 'Address that will be used for tax calculation', array('model' => 'tax/option_basis', 'translation_module' => 'Axis_Tax'))
-            ->add('shipping/Flat_Standard/taxClass', 'Tax Class', '', 'select', 'Tax class that will be used for tax calculation', array('model' => 'tax/option_class', 'translation_module' => 'Axis_Tax'))
-            ->add('shipping/Flat_Standard/sortOrder', 'Sort Order', '0', 'text', array('translation_module' => 'Axis_Core'))
-            ->add('shipping/Flat_Standard/multiPrice', 'Multi Price', '25', 'multiprice', '', array('model' => 'shippingFlat/option_standard_multiPrice'))
-            ->add('shipping/Flat_Standard/type', 'Type', Axis_ShippingFlat_Model_Option_Standard_Service::PER_ORDER, 'select', 'The shipping cost is based on:', array('model' => 'shippingFlat/option_standard_service'))
-            ->add('shipping/Flat_Standard/formDesc', 'Checkout Description', 'Flat Rate')
-            ->add('shipping/Flat_Standard/payments', 'Disallowed Payments', '0', 'multiple', 'Selected payment methods will be not available with this shipping method', array('model' => 'checkout/option_payment', 'translation_module' => 'Axis_Admin'))
-
+            ->add('shipping/Item_Standard', 'Shipping Methods/Item Standard', null, null, array('translation_module' => 'Axis_ShippingItem'))
+            ->add('shipping/Item_Standard/enabled', 'Shipping Methods/Item Standard/Enabled', '0', 'radio', '', array('model'=> 'core/option_boolean', 'translation_module' => 'Axis_Core'))
+            ->add('shipping/Item_Standard/geozone', 'Allowed Shipping Zone', '1', 'select', 'Shipping method will be available only for selected zone', array('model' => 'location/option_geozone', 'translation_module' => 'Axis_Admin'))
+            ->add('shipping/Item_Standard/taxBasis', 'Tax Basis', '', 'select', 'Address that will be used for tax calculation', array('model' => 'tax/option_basis', 'translation_module' => 'Axis_Tax'))
+            ->add('shipping/Item_Standard/taxClass', 'Tax Class', '', 'select', 'Tax class that will be used for tax calculation', array('model' => 'tax/option_class', 'translation_module' => 'Axis_Tax'))
+            ->add('shipping/Item_Standard/sortOrder', 'Sort Order', '0', 'text', array('translation_module' => 'Axis_Core'))
+            ->add('shipping/Item_Standard/price', 'Shipping Price', '1', 'text', 'Shipping price (per item)')
+            ->add('shipping/Item_Standard/payments', 'Disallowed Payments', '0', 'multiple', 'Selected payment methods will be not available with this shipping method', array('model' => 'checkout/option_payment', 'translation_module' => 'Axis_Admin'))
             ->transform()
 ;
          
