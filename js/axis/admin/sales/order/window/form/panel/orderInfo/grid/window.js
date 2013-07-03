0be8032288bbbe8249ca05b69b9317d600051d33
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2013-07-03 10:35:10 +0300

    Grid column width fixes

--- js/axis/admin/sales/order/window/form/panel/orderInfo/grid/window.js
+++ js/axis/admin/sales/order/window/form/panel/orderInfo/grid/window.js
@@ -285,24 +285,28 @@ Ext.onReady(function(){
             selectionModel,
             expander,
         {
+            align: 'right',
             header: 'Id'.l(),
             dataIndex: 'id',
-            menuDisabled: true
+            menuDisabled: true,
+            width: 80
         }, {
             header: 'Name'.l(),
             dataIndex: 'name',
-            width: 300,
+            id: 'name',
             menuDisabled: true,
             table: 'cpd'
         }, {
             header: 'SKU'.l(),
             dataIndex: 'sku',
-            menuDisabled: true
+            menuDisabled: true,
+            width: 110
         }, {
             align: 'right',
             header: 'Price'.l(),
             dataIndex: 'price',
-            menuDisabled: true
+            menuDisabled: true,
+            width: 80
         }, {
             align: 'right',
             header: 'Quantity'.l(),
@@ -314,7 +318,8 @@ Ext.onReady(function(){
                 allowNegative: false,
                 maxValue: 100000
             }),
-            filterable: false
+            filterable: false,
+            width: 80
         }, {
             align: 'right',
             header: 'Available'.l(),
@@ -323,7 +328,8 @@ Ext.onReady(function(){
             sortName: 'quantity',
             filter: {
                 name: 'quantity'
-            }
+            },
+            width: 80
         }]
     });
 
@@ -333,6 +339,7 @@ Ext.onReady(function(){
     });
 
     var gridAddProducts = new Axis.grid.EditorGridPanel({
+        autoExpandColumn: 'name',
         cm: cm,
         sm: selectionModel,
         ds: storeProducts,
@@ -343,7 +350,6 @@ Ext.onReady(function(){
         ],
         border: false,
         viewConfig: {
-            forceFit: true,
             emptyText: 'No records found'.l()
         },
         bbar: pagginator
